9c4aee4d0608b58d17ebc92d085432b6aba347ac
HSPRAFRIQUE <samuelivere92@gmail.com>
2025-11-13 15:50:37 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,jsx}"], // adjust paths to your HTML/JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
